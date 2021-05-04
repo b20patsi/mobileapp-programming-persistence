20de9c125c2f1b303275f6e8649404ba926085e2
Patrik Silfverberg <b20patsi@student.his.se>
2021-05-04 11:45:32 +0200

    Declare all widgets id

--- app/src/main/java/com/example/persistence/MainActivity.java
+++ app/src/main/java/com/example/persistence/MainActivity.java
@@ -4,11 +4,21 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     private SQLiteDatabase database;
     private DatabaseHelper databaseHelper;
+
+    private Button readBtn;
+    private Button writeBtn;
+    private TextView textView;
+    private EditText textName;
+    private EditText textAge;
+    private EditText textCity;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,5 +27,12 @@ public class MainActivity extends AppCompatActivity {
 
         databaseHelper = new DatabaseHelper(this);
         database = databaseHelper.getWritableDatabase();
+
+        readBtn=findViewById(R.id.read);
+        writeBtn=findViewById(R.id.write);
+        textView=findViewById(R.id.read_text);
+        textName=findViewById(R.id.name);
+        textAge=findViewById(R.id.age);
+        textCity=findViewById(R.id.city);
     }
 }
