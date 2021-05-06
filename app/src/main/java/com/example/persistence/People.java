9fdc38d46b497f2db419bdca7661034b4f9d0515
Patrik Silfverberg <b20patsi@student.his.se>
2021-05-06 11:15:55 +0200

    Updated strings

--- app/src/main/java/com/example/persistence/People.java
+++ app/src/main/java/com/example/persistence/People.java
@@ -5,7 +5,7 @@ public class People {
     private int age;
     private String city;
 
-    public People(String _name, String _city, int _age) {
+    public People(String _name, int _age, String _city) {
         name=_name;
         age=_age;
         city=_city;
