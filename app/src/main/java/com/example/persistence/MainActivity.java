932df17038946541431fdbd8c0daeb73c395e160
Patrik Silfverberg <b20patsi@student.his.se>
2021-05-06 09:47:44 +0200

    Able to add to database

--- app/src/main/java/com/example/persistence/MainActivity.java
+++ app/src/main/java/com/example/persistence/MainActivity.java
@@ -2,11 +2,18 @@ package com.example.persistence;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ContentValues;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,6 +26,15 @@ public class MainActivity extends AppCompatActivity {
     private EditText textName;
     private EditText textAge;
     private EditText textCity;
+
+    private long addPeople() {
+        ContentValues values = new ContentValues();
+        values.put(DatabaseTable.People.COLUMN_NAME_NAME, String.valueOf(textName.getText()));
+        values.put(DatabaseTable.People.COLUMN_NAME_AGE, String.valueOf(textAge.getText()));
+        values.put(DatabaseTable.People.COLUMN_NAME_CITY, String.valueOf(textCity.getText()));
+        Log.d("MainActivityAdd ==>", String.valueOf(textName.getText()) + ", " + String.valueOf(textAge.getText()) + ", " + String.valueOf(textCity.getText()));
+        return database.insert(DatabaseTable.People.TABLE_NAME, null, values);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,5 +50,19 @@ public class MainActivity extends AppCompatActivity {
         textName=findViewById(R.id.name);
         textAge=findViewById(R.id.age);
         textCity=findViewById(R.id.city);
+
+        writeBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+            addPeople();
+            Log.d("MainAcitivtyWrite ==>","Name: " + textName.getText() + ", Age: " + textAge.getText() + ", City: " + textCity.getText());
+            textName.setText("");
+            textAge.setText("");
+            textCity.setText("");
+            }
+        });
+
+
+
     }
 }
